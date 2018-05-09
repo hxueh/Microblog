6ec4c9ca6096b28dd9560e6ebab438f85fdb3ab4
Sharehong Wong <sharehong.wong@gmail.com>
2018-05-09 15:55:39 +0800

    PEP8.

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
-
+auth = Blueprint('auth', __name__)
 from . import views
 
 # http://flask.pocoo.org/docs/0.12/blueprints/#my-first-blueprint
-auth = Blueprint('auth', __name__)
